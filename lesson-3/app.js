025115e3e9fa08db396f823c19b83778bd486121
Temurbek Sadullayev <temurbeksadullaev55@gmail.com>
2025-06-02 20:24:59 +0500

    javascript-course-files

---
+++ lesson-3/app.js
@@ -0,0 +1,18 @@
+//? O'zgaruvchilarni e'lon qilish
+let ism = 'Temurbek';
+let yosh = 20;
+console.log(ism, yosh);
+
+//? O'zgaruvchilarni qayta e'lon qilish
+yosh = 21;
+console.log(yosh);
+
+//? O'zgaruvchilarni const bilan e'lon qilish
+const familiya = 'Sadullayev';
+console.log(familiya);
+//! familiya = 'Xudoyberdiyev'; // Bu xato, const o'zgaruvchisi qayta o'zgartirilmaydi
+
+//? O'zgaruvchilarni var bilan e'lon qilish
+var joy = 'Xorazm';
+joy = 'Xiva'; //! var o'zgaruvchisi qayta o'zgartirilishi mumkin
+console.log(joy);
